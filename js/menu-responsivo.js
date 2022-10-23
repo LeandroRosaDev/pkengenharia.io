4fe0631d07dc5c5ff92d2f38bcfad473341c2aaa
Leandro Rosa <leandrorosa592@gmail.com>
2022-10-23 08:01:35 -0300

    Atualização do menu

--- js/menu-responsivo.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".hamburguer").addEventListener("click", ()=>
- document.querySelector(".menu-hamburguer").classList.toggle("show-menu")
-);
-document.querySelector(".hamburguer").addEventListener("click", ()=>
- document.querySelector(".sidebar").classList.toggle("rigth-menu")
-);
